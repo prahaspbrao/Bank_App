e7ff5869a8596e1ab7c802a8e3885a30a38be334
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-02-22 21:34:33 +0530

    Model created

---
+++ backend/src/models/UserModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    email :  {
+        type : String,
+        required : true,
+        trim : true,
+        lower : true,
+        unique : true
+    },
+    password :  {
+        type : String,
+        required : true,
+    },
+    ac_type : {
+        type : String,
+        required : true,
+        enum : ['saving' ,'current'],
+        default : 'saving'
+    }
+} , {
+    timestamps : true
+})
+
+const model = mongoose.model("user" , schema);
+
+module.exports = model
